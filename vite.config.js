6704a33c1a9eb79f8ffaca15241d6976eb8907b0
orcutte <orcutte@sunypoly.edu>
2025-10-14 14:15:18 -0400

    configure base for project pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ethanorcutt.github.io/',  // Set the base path for the project
   build: {
     outDir: 'docs', // Specify the output directory as 'docs', instead of the default 'dist'
   },
